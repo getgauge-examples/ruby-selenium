756e8edd86454c038bdc873b190998ba4fbebfdd
Soumya <sswaroop@thoughtworks.com>
2017-07-18 16:03:47 +0530

    using the headless=N option

--- step_implementations/utils/app_launcher.rb
+++ step_implementations/utils/app_launcher.rb
@@ -6,5 +6,5 @@ include ::Driver
 
 step 'Go to the store website' do
   driver.navigate.to ENV['APP_URL']
-  assert_equal driver.title, 'The Active Admin Store'
+  assert_equal 'The Active Admin Store', driver.title
 end
